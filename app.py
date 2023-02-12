0b3cbbce7a5b819aa8013a0d0655cb686f162131
MadeInShineA <olivier.amacker@netplus.ch>
2023-02-12 18:12:27 +0100

    Upgraded the model and finished the project!

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import pygame
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+import os
 
-model = tf.keras.models.load_model('test-digits-model')
+model = tf.keras.models.load_model('final-cnn-digits-model')
 
 
 
@@ -12,6 +13,7 @@ screen = pygame.display.set_mode((1000, 500))
 
 drawing_area = pygame.Rect(500, 0, 500, 500)
 pygame.draw.rect(screen, "black", (500,0,500,500), 0)
+
 
 
 
@@ -28,56 +30,11 @@ basic_text_rect = basic_text_surface.get_rect(center=(500 / 2, 20))
 
 text.append((basic_text_surface,basic_text_rect))
 
-zero_text_surface = my_font.render('0 : ', True, "white")
-zero_text_rect = basic_text_surface.get_rect(center=(100, 40))
+for i in range(10):
+    surface = my_font.render(str(i)+" : ", True, "white")
+    rect = basic_text_surface.get_rect(center=(100, ((i+1)*30)+10))
+    text.append((surface, rect))
 
-text.append((zero_text_surface,zero_text_rect))
-
-one_text_surface = my_font.render('1 : ', True, "white")
-one_text_rect = basic_text_surface.get_rect(center=(100, 70))
-
-text.append((one_text_surface,one_text_rect))
-
-two_text_surface = my_font.render('2 : ', True, "white")
-two_text_rect = basic_text_surface.get_rect(center=(100, 100))
-
-text.append((two_text_surface,two_text_rect))
-
-tree_text_surface = my_font.render('3 : ', True, "white")
-tree_text_rect = basic_text_surface.get_rect(center=(100, 130))
-
-text.append((tree_text_surface,tree_text_rect))
-
-four_text_surface = my_font.render('4 : ', True, "white")
-four_text_rect = basic_text_surface.get_rect(center=(100, 160))
-
-text.append((four_text_surface,four_text_rect))
-
-
-five_text_surface = my_font.render('5 : ', True, "white")
-five_text_rect = basic_text_surface.get_rect(center=(100, 190))
-
-text.append((five_text_surface,five_text_rect))
-
-six_text_surface = my_font.render('6 : ', True, "white")
-six_text_rect = basic_text_surface.get_rect(center=(100, 220))
-
-text.append((six_text_surface,six_text_rect))
-
-seven_text_surface = my_font.render('7 : ', True, "white")
-seven_text_rect = basic_text_surface.get_rect(center=(100, 250))
-
-text.append((seven_text_surface,seven_text_rect))
-
-eight_text_surface = my_font.render('8 : ', True, "white")
-eight_text_rect = basic_text_surface.get_rect(center=(100, 280))
-
-text.append((eight_text_surface,eight_text_rect))
-
-nine_text_surface = my_font.render('9 : ', True, "white")
-nine_text_rect = basic_text_surface.get_rect(center=(100, 310))
-
-text.append((nine_text_surface,nine_text_rect))
 def display_text ():
     for surface, rect in text:
         screen.blit(surface, rect)
@@ -89,7 +46,8 @@ def save_image():
 def predict():
     img = Image.open("image.jpg").convert('L').resize((28, 28))
     img = np.array(img)
-    predictions = model.predict(img[None, :, :])
+    img = img/255
+    predictions = model.predict(img[None, :, :], verbose=0)
     return predictions
 def update_predictions(predictions):
     index = 0
@@ -110,24 +68,29 @@ def update_predictions(predictions):
 
 
 while game_running:
-    pygame.draw.rect(screen, "black", (0, 0, 500, 500), 0)
+    pygame.draw.rect(screen, "black", (0, 0, 480, 500), 0)
     display_text()
+    pygame.draw.rect(screen, "white", (480, 0, 20, 500), 0)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            if os.path.exists("image.jpg"):
+                os.remove("image.jpg")
             game_running = False
         if event.type == pygame.MOUSEBUTTONUP:
-            save_image()
-            predictions = predict()
-            update_predictions(predictions)
+            mouse_position = pygame.mouse.get_pos()
+            if drawing_area.collidepoint(mouse_position):
+                save_image()
+                predictions = predict()
+                update_predictions(predictions)
     if pygame.mouse.get_pressed() == (1, 0, 0):
         mouse_position = pygame.mouse.get_pos()
         if drawing_area.collidepoint(mouse_position):
-            pygame.draw.circle(surface=screen, center=mouse_position, color="white", radius=7)
+            pygame.draw.circle(surface=screen, center=mouse_position, color="white", radius=12)
     if pygame.mouse.get_pressed() == (0, 0, 1):
         mouse_position = pygame.mouse.get_pos()
         if drawing_area.collidepoint(mouse_position):
-            pygame.draw.circle(surface=screen, center=mouse_position, color="black", radius=10)
+            pygame.draw.circle(surface=screen, center=mouse_position, color="black", radius=12)
     if pygame.mouse.get_pressed() == (0, 1, 0):
         mouse_position = pygame.mouse.get_pos()
         if drawing_area.collidepoint(mouse_position):
--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from PIL import Image
 import numpy as np
 class Mycallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
-        if logs.get("accuracy") > 0.95:
+        if logs.get("accuracy") > 0.97:
             print("\nReached 95% accuracy so cancelling training!")
             self.model.stop_training = True
 
@@ -16,7 +16,6 @@ data = tf.keras.datasets.mnist
 
 train_datagen = ImageDataGenerator(
     rescale = 1/255,
-    rotation_range=20,
     width_shift_range=0.3,
     height_shift_range=0.3
 )
@@ -24,7 +23,6 @@ train_datagen = ImageDataGenerator(
 
 validation_datagen = ImageDataGenerator(
     rescale=1/255,
-    rotation_range=20,
     width_shift_range=0.3,
     height_shift_range=0.3)
 
@@ -39,20 +37,22 @@ train_generator = train_datagen.flow((training_images,training_labels),batch_siz
 ##train_data_gen.fit(test_images)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
-    tf.keras.layers.Dense(128, activation=tf.nn.relu),
-    tf.keras.layers.Dense(32, activation=tf.nn.relu),
+    tf.keras.layers.Conv2D(16, (3,3), activation="relu", input_shape=(28, 28, 1)),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(16, (3,3), activation="relu"),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(132, activation=tf.nn.relu),
     tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
 model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
 
-model.fit(train_generator,validation_data=validation_generator,steps_per_epoch = len(training_images) //32 , epochs=50, callbacks=[callback])
+model.fit(train_generator,validation_data=validation_generator,steps_per_epoch = len(training_images) //32 , epochs=100, callbacks=[callback])
 
 model.evaluate(test_images,test_labels)
 
-model.save("test-digits-model")
-img = Image.open("screenshot2.jpg").convert('L').resize((28, 28), Image.ANTIALIAS)
-img = np.array(img)
-print(model.predict(img[None,:,:]))
+model.save("final-cnn-digits-model")
 
 
